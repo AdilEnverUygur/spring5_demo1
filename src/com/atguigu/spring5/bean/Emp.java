7857ba298c441b35a085782fc1e95b15f7ea9ca7
Adil <Adiljan.anwar@gmail.com>
2022-08-29 14:00:49 +0300

    Inject Internal Bean

--- src/com/atguigu/spring5/bean/Emp.java
+++ src/com/atguigu/spring5/bean/Emp.java
@@ -6,6 +6,11 @@ public class Emp {
     private String gender;
     //员工属于某一个部门,使用对象形式表示
     private  Dept dept;
+    //生成dept的get方法
+
+    public Dept getDept() {
+        return dept;
+    }
 
     public void seteName(String eName) {
         this.eName = eName;
--- src/com/atguigu/spring5/testdemo/TestBean.java
+++ src/com/atguigu/spring5/testdemo/TestBean.java
@@ -23,7 +23,7 @@ public class TestBean {
     @Test
     public void testBean2(){
         //1.加载spring的配置文件
-        ApplicationContext context = new ClassPathXmlApplicationContext("bean3.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean4.xml");
 
         //2.获取配置创建的对象
         Emp emp = context.getBean("emp", Emp.class);
